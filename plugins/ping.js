3d434d18b1c2d873ded3a53e7cb70c324aa0b213
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2026-01-24 16:56:28 -0500

    Actualizar ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -2,7 +2,7 @@ import { performance } from 'perf_hooks'
 
 let handler = async (m, { conn }) => {
     let start = performance.now()
-    let { key } = await conn.sendMessage(m.chat, { text: '🚀 *Probando conexión...*' }, { quoted: m })
+    let { key } = await conn.sendMessage(m.chat, { text: '🚀 *Midiendo latencia...*' }, { quoted: m })
     let end = performance.now()
     let speed = (end - start).toFixed(2)
 
@@ -12,5 +12,13 @@ let handler = async (m, { conn }) => {
     })
 }
 
+handler.help = ['ping']
+handler.tags = ['main']
 handler.command = /^(ping)$/i
+
+// Propiedades de acceso
+handler.group = false    // Funciona en todos lados
+handler.admin = false    // No requiere ser admin
+handler.botAdmin = false // El bot no necesita ser admin
+
 export default handler
